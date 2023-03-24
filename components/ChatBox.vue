528c65137d965f8eb0e87bc34ee42d85be5e5b5b
TH <th@THs-MacBook-Air.local>
2023-03-24 10:50:01 -0700

    add gpt 3.5 and connect to crypto api

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
 		<div class="sticky top-0 left-0 w-full bg-gray-200 p-6 grid grid-cols-[auto_100px] gap-3">
-			<input v-model="chatInput" v-on:keyup.enter.prevent="askGPT" type="text" minlength="3" placeholder="Ask something..." class="p-2 rounded resize-none outline-0" />
-			<button @click="askGPT" class="px-4 py-2 font-bold text-xs text-white bg-cyan-500 rounded-md transition duration-300 ease-in-out hover:bg-cyan-400">Send</button>
+			<input v-model="chatInput" v-on:keyup.enter.prevent="sendMessage" type="text" minlength="3" placeholder="Ask something..." class="p-2 rounded resize-none outline-0" />
+			<button @click="sendMessage" class="px-4 py-2 font-bold text-xs text-white bg-cyan-500 rounded-md transition duration-300 ease-in-out hover:bg-cyan-400">Send</button>
 		</div>
 		<div class="m-4 mt-8 grid grid-cols-1 gap-4">
 			<template v-for="message in messages">
@@ -18,12 +18,37 @@
 const chatInput = ref('');
 const res = ref('');
 const messages: any = ref([]);
+const pricesList = ref('');
+const chatInputMod = ref('');
 
+// Make GPT interact with data from another API
+async function getCryptoData() {
+	const getCrypto = $fetch('https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history', {
+		method: "GET",
+		params: {"referenceCurrencyUuid": "yhjMzLPhuIDl", "timePeriod": "7d"},
+		headers: {
+			"X-RapidAPI-Key": "a617d6467dmshac84323ce581a72p11caa9jsn1adf8bbcbd47",
+			"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+		}
+	})
 
+	const cryptoData = await getCrypto;
+	let prices: any[] = [];
+
+	for (let history of cryptoData.data.history){
+  		prices.push(history.price);
+	}
+
+	pricesList.value = prices.join(',');
+	chatInputMod.value = `Hello ChatGPT! Could you provide a brief analysis based on the recent Bitcoin prices over the last seven days? I'll share the price data with you, and I'd love to hear your insights on what this could mean for the cryptocurrency market. Thanks in advance for your help! price list: ${pricesList.value}`
+}
+
+// Send question to GPT
 async function askGPT() {
 
-	const messageLength = parseInt(chatInput?.value?.length as any);
-	
+	const question = chatInputMod.value !== '' ? chatInputMod.value : chatInput.value;
+	const messageLength = parseInt(question.length as any);
+
 	if (messageLength < 2) {
 		return false;
 	}
@@ -33,11 +58,11 @@ async function askGPT() {
 		data: chatInput.value
 	})
 
-	const message = chatInput.value;
+	const message = question;
 	const response = $fetch('/api/chat', {
         method: 'post',
         body: { 
-			message 
+			message: question 
 		}
     })
     const result: any = await response
@@ -46,5 +71,15 @@ async function askGPT() {
 		from: 'gpt',
 		data: result.text?.slice(0)
 	})     
+}
+
+async function sendMessage() {
+	const isCrypto = chatInput?.value?.includes("crypto");
+	console.log(isCrypto);
+	if (isCrypto) {
+		getCryptoData().then(response => askGPT());
+	} else {
+		askGPT();
+	}
 }
 </script>
--- server/api/chat.js
+++ server/api/chat.js
@@ -15,17 +15,31 @@ export default defineEventHandler(async (event) => {
 
 	try {
 
-		const response = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: prompt,
-			temperature: 0.5,
-			max_tokens: 150,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
+		// Use gpt-3.5-turbo model
+		const response = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{
+					role: "user",
+					content: prompt
+				}
+			]
 		})
+		
+		// Use text-davinci-003 model
+		// const response = await openai.createCompletion({
+		// 	model: "text-davinci-003",
+		// 	prompt: prompt,
+		// 	temperature: 0.5,
+		// 	max_tokens: 150,
+		// 	top_p: 1,
+		// 	frequency_penalty: 0,
+		// 	presence_penalty: 0
+		// })
 
-		chat_reply = response.data.choices[0].text
+		// chat_reply = response.data.choices[0].text
+
+		chat_reply = response.data.choices[0].message.content
 
 	} catch (error) {
 
