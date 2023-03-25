22690bc4223ffd967c3663763dc61e2d9a23b3fa
TH <th@THs-MacBook-Air.users.gowaveg.com>
2023-03-25 08:30:20 -0700

    invert chat display no scrolling

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -4,9 +4,9 @@
 			<input v-model="chatInput" v-on:keyup.enter.prevent="sendMessage" type="text" minlength="3" placeholder="Ask something..." class="p-2 rounded resize-none outline-0" />
 			<button @click="sendMessage" class="px-4 py-2 font-bold text-xs text-white bg-cyan-500 rounded-md transition duration-300 ease-in-out hover:bg-cyan-400">Send</button>
 		</div>
-		<div class="m-4 mt-8 grid grid-cols-1 gap-4">
+		<div class="m-4 mt-8 flex flex-col-reverse flex-wrap gap-4">
 			<template v-for="message in messages">
-				<div :class="[message.from === 'user' ? 'justify-end' : 'justify-start']" class="flex">
+				<div :class="[message.from === 'user' ? 'justify-end' : 'justify-start']" class="w-full flex">
 					<div :class="[message.from === 'user' ? 'bg-gray-100' : 'bg-pink-200']" class="rounded-md p-3 max-w-[90%]">{{ message.data }}</div>
 				</div>
     		</template>
