cd8cbf953aea025e66be4cb34ce0751c92da7677
TH <tomjph@gmail.com>
2023-04-11 09:30:43 -0700

    user faster model for tests

--- server/api/chat.js
+++ server/api/chat.js
@@ -16,32 +16,32 @@ export default defineEventHandler(async (event) => {
 	try {
 
 		// Use gpt-3.5-turbo model
-		const response = await openai.createChatCompletion({
-			model: "gpt-3.5-turbo",
-			messages: [
-				{
-					role: "user",
-					content: prompt
-				}
-			],
-			temperature: 0.5,
-			max_tokens: 2000
-		})
+		// const response = await openai.createChatCompletion({
+		// 	model: "gpt-3.5-turbo",
+		// 	messages: [
+		// 		{
+		// 			role: "user",
+		// 			content: prompt
+		// 		}
+		// 	],
+		// 	temperature: 0.5,
+		// 	max_tokens: 2000
+		// })
 		
 		// Use text-davinci-003 model
-		// const response = await openai.createCompletion({
-		// 	model: "text-davinci-003",
-		// 	prompt: prompt,
-		// 	temperature: 0.5,
-		// 	max_tokens: 150,
-		// 	top_p: 1,
-		// 	frequency_penalty: 0,
-		// 	presence_penalty: 0
-		// })
+		const response = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: prompt,
+			temperature: 0.5,
+			max_tokens: 150,
+			top_p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0
+		})
 
-		// chat_reply = response.data.choices[0].text
+		chat_reply = response.data.choices[0].text
 
-		chat_reply = response.data.choices[0].message.content
+		// chat_reply = response.data.choices[0].message.content
 
 	} catch (error) {
 
